d73a6db5dbe24f9935ef5018d3cd3b3c9f16d764
jordan koichiro johnson <mrjordanjohnson@hotmail.com>
2022-10-05 18:53:37 -0700

    finished local storage

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var today = moment()
 
 var hour = moment().format("H")
 
-
+var dailyActivities = []
 
 $("#currentDay").text(today.format("MMM, Do, YYYY, h A"))
 
@@ -18,7 +18,7 @@ while (timeClass < hour) {
     
     $(`#${timeClass}`).css('background-color', 'silver')
 
-    console.log(timeClass)
+  
     
     
     timeClass ++
@@ -28,7 +28,7 @@ timeClass ++
 
 while (timeClass > hour && timeClass < 18) {
     $(`#${timeClass}`).css('background-color', 'red')
-    console.log(timeClass);
+   
     timeClass ++
 }
 
@@ -56,15 +56,47 @@ while (timeClass > hour && timeClass < 18) {
 // }
 
 function saveEvent(eventObj) {
-    console.log(eventObj.name)
+    // console.log(eventObj.name)
+    var eventAtTime = eventObj.value
+    // console.log(eventAtTime)
+    // console.log(eventObj.parentElement)
+    // let pEl = document.createElement("p")
+    // eventObj.parentElement.prepend(pEl)
+    // pEl.innerText = eventAtTime
+    // console.log(pEl.innerText)
+    let dailyActivitiesObject = {
+        time: eventObj.name,
+        activity: eventAtTime
+    }
+    dailyActivities.unshift(dailyActivitiesObject)
+    console.log(dailyActivities)
+    localStorage.setItem(dailyActivities[0].time, dailyActivities[0].activity)
+    localStorage.removeItem(dailyActivities[0])
+}
+
+
+function weFocusing(focusThing) {
+    focusThing.focus()
+}
+
+for  (i=9; i<18; i++){
+
+
+    let storageActivities = localStorage.getItem(`${i}Event`)
+    console.log(storageActivities)
+    let classActivities = $(`#${i}Event`)[0]
+    console.log(classActivities)
+    classActivities.value = storageActivities
 }
 
 for  (i=9; i<18; i++) {
     (function () {
         var b = $(`.${i}button`)[0]
-        console.log(b)
+      
         var e = $(`#${i}Event`)[0]
         b.addEventListener("click", function () {saveEvent(e);}, false)
-        console.log("function ran")
-    }());
+    
+        e.parentElement.addEventListener("click", function () {weFocusing(e);}, false)
+    }())
 }
+
