a4ae6600e58a0353e954a2ada4fa66f8d933ea5d
jordan koichiro johnson <mrjordanjohnson@hotmail.com>
2022-10-06 11:10:39 -0700

    refactored code a bit

--- script.js
+++ script.js
@@ -1,25 +1,17 @@
-
-
 var today = moment()
-
 var hour = moment().format("H")
-
 var dailyActivities = []
 
 $("#currentDay").text(today.format("MMM, Do, YYYY, h A"))
 
 $(`#${hour}`).css('background-color', 'green')
 
-
 var timeClass = 9;
 
+// set background equal to color depending on time
 
 while (timeClass < hour) {
-    
     $(`#${timeClass}`).css('background-color', 'silver')
-
-  
-    
     
     timeClass ++
 }
@@ -32,70 +24,40 @@ while (timeClass > hour && timeClass < 18) {
     timeClass ++
 }
 
-
-
-// for (i=9; i < 18; i++) {
-    
-//     var buttonClass = i;
-    
-//     $(`.${i}button`).on("click", saveClick)
-
-// }
-
-// function saveClick(event) {
-    
-//     event.preventDefault
-//     console.log("click")
-//     console.log(event)
-    
-//     let currentTime = event.currentTarget.name
-//     console.log(currentTime)
-//     var eventAtTime = $(`#${buttonClass}Event`).val()
-//     console.log(`#${buttonClass}Event`)
-//     console.log(eventAtTime);
-// }
+// put existing value into an array which is saved to local storage
 
 function saveEvent(eventObj) {
-    // console.log(eventObj.name)
     var eventAtTime = eventObj.value
-    // console.log(eventAtTime)
-    // console.log(eventObj.parentElement)
-    // let pEl = document.createElement("p")
-    // eventObj.parentElement.prepend(pEl)
-    // pEl.innerText = eventAtTime
-    // console.log(pEl.innerText)
     let dailyActivitiesObject = {
         time: eventObj.name,
         activity: eventAtTime
     }
     dailyActivities.unshift(dailyActivitiesObject)
-    console.log(dailyActivities)
     localStorage.setItem(dailyActivities[0].time, dailyActivities[0].activity)
     localStorage.removeItem(dailyActivities[0])
 }
 
+// focus on form on div click
 
 function weFocusing(focusThing) {
     focusThing.focus()
 }
 
+//add local storage to page on page load
+
 for  (i=9; i<18; i++){
-
-
     let storageActivities = localStorage.getItem(`${i}Event`)
-    console.log(storageActivities)
-    let classActivities = $(`#${i}Event`)[0]
-    console.log(classActivities)
+    let classActivities = $(`#${i}Event`)[0] 
     classActivities.value = storageActivities
 }
+
+// create event listeners
 
 for  (i=9; i<18; i++) {
     (function () {
         var b = $(`.${i}button`)[0]
-      
         var e = $(`#${i}Event`)[0]
         b.addEventListener("click", function () {saveEvent(e);}, false)
-    
         e.parentElement.addEventListener("click", function () {weFocusing(e);}, false)
     }())
 }
